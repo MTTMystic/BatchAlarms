4c086f2bc677a548d9b8e894767f2c43b7eae654
MTTMystic <mttmystic@gmail.com>
2026-03-24 17:03:46 -0500

    alarm scheduler computeNextAlarmTime

--- app/src/main/java/mttmystic/batchAlarms/AlarmScheduler.kt
+++ app/src/main/java/mttmystic/batchAlarms/AlarmScheduler.kt
@@ -1,13 +1,16 @@
 package mttmystic.batchAlarms
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import dagger.hilt.android.qualifiers.ApplicationContext
 import jakarta.inject.Inject
 import java.time.DayOfWeek
+import java.time.LocalTime
+import java.time.ZonedDateTime
 
 interface AlarmScheduler {
 
-    fun scheduleAlarm(id: Int, hour : Int, minute : Int, repeatDays : Set<DayOfWeek>)
+    fun scheduleAlarm(id: Int, hour : Int, minute : Int, repeatDays : Set<DayOfWeek> = emptySet())
 
     fun cancelAlarm(id: Int)
 }
@@ -16,6 +19,36 @@ interface AlarmScheduler {
 class AlarmSchedulerImpl @Inject constructor (
     @ApplicationContext private val  context : Context
 ) : AlarmScheduler {
+
+    @VisibleForTesting
+    internal fun computeNextAlarmTime(
+        hour: Int,
+        minute: Int,
+        repeatDays : Set<DayOfWeek>,
+        now : ZonedDateTime = ZonedDateTime.now()) : ZonedDateTime {
+        val localTime = LocalTime.of(hour, minute, 0, 0)
+        //val now = ZonedDateTime.now()
+        val todayAt = now.with(localTime)
+        lateinit var nextTime : ZonedDateTime
+        if (repeatDays.isEmpty()) {
+            nextTime = if
+                    (!todayAt.isAfter(now)) {
+                        todayAt.plusDays(1)
+                    } else {
+                        todayAt
+                    }
+
+        }
+
+        for (day in (0..7)) {
+            val candidate = todayAt.plusDays(day.toLong())
+            if (repeatDays.contains(candidate.dayOfWeek) && candidate.isAfter(now)) {
+                nextTime = candidate
+            }
+        }
+
+        return nextTime
+    }
 
     override fun scheduleAlarm(id: Int, hour : Int, minute : Int, repeatDays : Set<DayOfWeek>) {}
 
---
+++ app/src/test/java/mttmystic/batchAlarms/AlarmSchedulerTest.kt
@@ -0,0 +1,9 @@
+package mttmystic.batchAlarms
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+class AlarmSchedulerTest {
+    val now = ZonedDateTime.of(2026, 3, 24, 5, 0, 0, 0, ZoneId.systemDefault())
+
+}
