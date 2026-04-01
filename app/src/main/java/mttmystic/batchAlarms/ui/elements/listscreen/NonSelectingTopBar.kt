4c784a94a2995c3ab006a5075bd026b0ae1f35b2
MTTMystic <mttmystic@gmail.com>
2026-04-01 09:00:26 -0500

    non selection mode top bar

--- app/src/main/java/mttmystic/batchAlarms/ui/elements/listscreen/NonSelectingTopBar.kt
+++ app/src/main/java/mttmystic/batchAlarms/ui/elements/listscreen/NonSelectingTopBar.kt
@@ -1,2 +1,46 @@
 package mttmystic.batchAlarms.ui.elements.listscreen
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NonSelectingTopBar(
+    //TODO only display this on selection mode -> select all
+    onClickDelete : () -> Unit = {},
+    onClickSettings : () -> Unit = {}
+) {
+    TopAppBar(
+        //TODO str resource
+        title = { Text("Batch Alarms") },
+        actions = {
+            IconButton(onClick = onClickDelete) {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "cancel all alarms"
+                )
+            }
+            IconButton(onClick = onClickSettings) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "go to settings"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
